695ca7a5f6f8318f059add487ace90ad50d02980
DerhaloNick <112712947+DerhaloNick@users.noreply.github.com>
2022-09-04 21:06:16 +0300

    Add files via upload
    Правильне виконання домашнього завдання 1_1

---
+++ HOMEWORK1/HomeWork1_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // a,b as long of square
+            string name = Console.ReadLine();
+            Console.Write("Enter a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Mult {a}*{b}={a * b}");
+
+        Start1:
+            Console.WriteLine("Do you want to diside a perimeter of this sqere?(YES or NO)");
+            string answer = Console.ReadLine();
+            if (answer != " YES ")
+            {
+                double c = a == b ? a * 4 : a * 2 + b * 2;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("End the Program? Tab anything");
+                Console.WriteLine();
+            };
+            //else
+            //{
+            //    Console.WriteLine(("Wrong answer.Start Againe"));
+            //    Console.ReadKey();
+            //    goto Start1;
+            //}
+        }
+    }
+}
